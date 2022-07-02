15dbc265497172fd00d96e76e9f8d602cb56defb
Muhammad Labib <99881858+labibofficial21@users.noreply.github.com>
2022-07-02 12:54:12 +0700

    final_file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Routes from "./Router/Routes";
+// import Registration from './Components/Login/Registration';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Routes></Routes>
+    </React.Fragment>
+  );
+}
+
+export default App;
